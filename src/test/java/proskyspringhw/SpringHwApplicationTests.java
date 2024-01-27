b7c4ecc40c34bcbc769479a242027c7b928fe03a
Танин <mazayztn@yandex.ru>
2024-01-27 16:19:12 +0300

    initial commit

---
+++ src/test/java/proskyspringhw/SpringHwApplicationTests.java
@@ -0,0 +1,13 @@
+package proskyspringhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
